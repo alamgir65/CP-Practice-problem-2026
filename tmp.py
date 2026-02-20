173de44c3e9267a806ba69ae4c17b1ba25e7e4c6
unknown <alamgirhossaindrubo@gmail.com>
2026-02-20 23:58:31 +0600

    hdh

--- tmp.py
+++ tmp.py
@@ -2,25 +2,24 @@ import pyautogui
 import time
 
 sentence = "Swarna tumi amare kobe nije theke message diba? bolo...."
-n = 50
-sentences = [
-    "Ki bhabe kotha bolcho? Eta ki bhodrota?",
-    "Beyadobi korteso je?",
-    "Evabe kotha bola ekdom thik na.",
-    "Bujhlam na, adob-kaida shikho nai naki?",
-    "Boro der shathe evabe kotha bola jay?",
-    "Kotha bolar age duibar bhabo.",
-    "Achoron thik koro,",
-    "Eta shesh barer moto bolchi..",
-    "Bhodro vabe kotha bolte shekho.",
-    "Tomar bebohar ekdom grohonjoggo na apni/tumi bolo.",
-    "Nijeke thik koro age, tarpor kotha bolo."
+n = 100
+shayari = [
+    "Kabhi kabhi kisi ka intezar bhi insaan ko khud se mila deta hai.",
+    "Tum se baat ho ya na ho, magar tumhara khayal rehna zaroori hai.",
+    "Khamoshi bhi ajeeb hoti hai, dil ki baatein keh jaati hai.",
+    "Kuch log zindagi mein aise milte hain, jinse kam baat ho kar bhi lagav ho jata hai.",
+    "Intezaar karna aadat nahi meri, bas tum par yakeen zyada hai.",
+    "Dil se nikli hui baat asar karti hai, warna kaghaz par har koi likh leta hai.",
+    "Kuch ehsaas lafzon mein bayan nahi hote, bas mehsoos kiye jaate hain.",
+    "Tumhari ek chhoti si baat bhi din ko behtar bana deti hai.",
+    "Har mulaqat zaroori nahi hoti, kuch rishte ehsaas se bhi chalte hain.",
+    "Kabhi kabhi chup rehna bhi behtareen jawab hota hai."
 ]
 
 print("Place your cursor where you want to type...")
 time.sleep(5)
 
 for i in range(n):
-    pyautogui.write(sentences[i % len(sentences)])
+    pyautogui.write(shayari[i % len(shayari)])
     pyautogui.press("enter")
     time.sleep(0.1)
