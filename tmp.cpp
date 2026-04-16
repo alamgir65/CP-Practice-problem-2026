8647843107497d3d623ae3ea631097a85d8e7f63
unknown <alamgirhossaindrubo@gmail.com>
2026-04-16 23:43:09 +0600

    nothing

--- tmp.cpp
+++ tmp.cpp
@@ -1,73 +1,19 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-#define optimize() ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
-#define nl "\n"
-#define rep(i, a, b) for(int i = (a); i < (b); i++)
-#define rrep(i, a, b) for(int i = (a); i >= (b); i--)
-
-typedef long long ll;
-typedef vector <int> vi;
-typedef vector <double> vd;
-typedef vector <ll> vll;
-
-void solve()
+int main()
 {
-    ll t,h,u, ans=0;
-    cin >> t >> h >> u;
-
-    ll tmp = min({t,h,u});
-    ans += tmp*7LL;
-    t-=tmp, h-=tmp, u-=tmp;
-
-    if(h==0)
-    {
-        tmp = min(t,u);
-        ans += tmp*4LL;
-        t-=tmp, u-=tmp;
-        if(t==0)
-        {
-            ans += u*3LL;
-        }   
-        else if(u==0 && t)
-        {
-            // ans += (3+(2*(t-1)));
-            ans += 2LL*t + 1;
-        }     
+    int n; cin>>n;
+    int cnt=0,n2=n;
+    int x=0;
+    vector<int> v;
+    while(n != 0){
+        cnt++;
+        v.push_back(n%10);
+        n /= 10;
     }
-    else if(u==0)
-    {
-        tmp = min(t/2, h);
-        ans += tmp*7LL;
-        t -= tmp*2, h-=tmp;
-
-        
-        tmp = min(t,h);
-        ans += tmp*5LL;
-        t -= tmp, h-=tmp;
-
-        if(t==0)
-            ans += h*3LL;
-        else if(t)
-            ans += 3+(2LL*(t-1));
-    }
-    else if(t==0)
-    {
-        ans += (h+u)*3LL;
-    }
-
-    cout << ans << endl;
-}
-
-int main ()
-{
-    optimize();
-              
-    int t = 1;
-    cin >> t;
-
-    while(t--)
-        solve();
-        
+    for(int u:v) x += pow(u,cnt), cout<<x<<" ";
+    cout<<x<<endl;
+    (x == n2)? cout<<"Armstong Number" : cout<<"Not Armstong";
     return 0;
 }
